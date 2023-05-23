0007adc0032bf468010ee69ef5a9e36429af8aaa
Roman Ludwig <48687784+rmnldwg@users.noreply.github.com>
2023-05-23 19:22:09 +0200

    attempt to get pipeline running

---
+++ 2022-clb-multisite/render.py
@@ -0,0 +1,19 @@
+"""
+Short module to render the `README.md` file from the `README.template` file and
+the `mapping.py`.
+"""
+from pathlib import Path
+from jinja2 import Environment, FileSystemLoader
+
+from lyscripts.data.lyproxify import generate_markdown_docs
+
+from mapping import COLUMN_MAP
+
+if __name__ == "__main__":
+    parent_dir = Path(__file__).parent
+    env = Environment(loader=FileSystemLoader(parent_dir))
+    template = env.get_template("README.template")
+    result = template.render(description=generate_markdown_docs(COLUMN_MAP))
+
+    with open(parent_dir / "README.md", "w") as f:
+        f.write(result)
