1c61f46c8b14c2f63feaff5a88e68dd3af1272be
Roman Ludwig <48687784+rmnldwg@users.noreply.github.com>
2024-05-27 15:53:30 +0200

    getting data from github (WIP)

--- lydata/loader.py
+++ lydata/loader.py
@@ -1,12 +1,17 @@
 """Module for loading the lydata datasets."""
+from io import TextIOWrapper
+import os
+import logging
+import fnmatch
 from pathlib import Path
-from typing import Generator
+from typing import Generator, Literal
 from dataclasses import dataclass, field
 
 import mistletoe
 from mistletoe.block_token import Heading
 from mistletoe.markdown_renderer import MarkdownRenderer
 import pandas as pd
+from github import Github, Auth
 
 from lydata import _repo
 
@@ -45,15 +50,19 @@ class DatasetSpec:
             f"{self.year}-{self.institution}-{self.subsite}/data.csv"
         )
 
-    def load(self, **load_kwargs) -> pd.DataFrame:
-        """Load the dataset."""
+    def _load_or_fetch(self, loc: Path | str, **load_kwargs) -> pd.DataFrame:
+        # pylint: disable=logging-fstring-interpolation
         kwargs = {"header": [0, 1, 2]}
         kwargs.update(load_kwargs)
-        return pd.read_csv(self.path / "data.csv", **kwargs)
+        return pd.read_csv(loc, **kwargs)
+
+    def load(self, **load_kwargs) -> pd.DataFrame:
+        """Load the dataset."""
+        return self._load_or_fetch(self.path, **load_kwargs)
 
     def fetch(self, **load_kwargs) -> pd.DataFrame:
         """Fetch the dataset from the web."""
-        return pd.read_csv(self.url, **load_kwargs)
+        return self._load_or_fetch(self.url, **load_kwargs)
 
 
 def remove_subheadings(elements: list, min_level: int = 1) -> list:
@@ -69,7 +78,7 @@ def remove_subheadings(elements: list, min_level: int = 1) -> list:
 
 
 def get_description(
-    path: str | Path,
+    readme: TextIOWrapper | str,
     short: bool = False,
     max_line_length: int = 60,
 ) -> str:
@@ -82,8 +91,7 @@ def get_description(
         max_line_length=max_line_length,
         normalize_whitespace=True,
     ) as renderer:
-        with open(path, mode="r", encoding="utf-8") as file:
-            doc = mistletoe.Document(file)
+        doc = mistletoe.Document(readme)
 
         if short:
             doc.children = remove_subheadings(doc.children, min_level=1)
@@ -91,19 +99,11 @@ def get_description(
         return renderer.render(doc)
 
 
-def available_datasets(
+def _available_datasets_on_disk(
     year: int | str = "*",
     institution: str = "*",
     subsite: str = "*",
 ) -> Generator[DatasetSpec, None, None]:
-    """Generate names of available datasets.
-
-    >>> [ds.name for ds in available_datasets()]   # doctest: +NORMALIZE_WHITESPACE
-    ['2021-usz-oropharynx',
-     '2021-clb-oropharynx',
-     '2023-clb-multisite',
-     '2023-isb-multisite']
-    """
     year = str(year)
     search_path = Path(__file__).parent.parent
 
@@ -119,6 +119,64 @@ def available_datasets(
                 path=match,
                 description=description,
             )
+
+
+def _get_github_auth() -> Auth:
+    token = os.getenv("GITHUB_TOKEN")
+    user = os.getenv("GITHUB_USER")
+    password = os.getenv("GITHUB_PASSWORD")
+
+    if token:
+        return Auth.Token(token)
+
+    if user and password:
+        return Auth.Login(user, password)
+
+    raise ValueError("Neither GITHUB_TOKEN nor GITHUB_USER and GITHUB_PASSWORD are set.")
+
+
+def _available_datasets_on_github(
+    year: int | str = "*",
+    institution: str = "*",
+    subsite: str = "*",
+    repo: str = _repo,
+    # revision: str = "main",   # TODO: Add revision parameter
+) -> Generator[DatasetSpec, None, None]:
+    github = Github(auth=_get_github_auth())
+
+    repo = github.get_repo(repo)
+    contents = repo.get_contents("")
+
+    matches = []
+    for content in contents:
+        if (
+            content.type == "dir"
+            and fnmatch.fnmatch(content.name, f"{year}-{institution}-{subsite}")
+        ):
+            matches.append(content)
+
+    for match in matches:
+        readme = repo.get_contents(f"{match.path}/README.md")
+        description = 
+        data = repo.get_contents(f"{match.path}/data.csv")
+
+
+def available_datasets(
+    year: int | str = "*",
+    institution: str = "*",
+    subsite: str = "*",
+    where: Literal["disk", "github"] = "disk",
+) -> Generator[DatasetSpec, None, None]:
+    """Generate names of available datasets.
+
+    >>> [ds.name for ds in available_datasets()]   # doctest: +NORMALIZE_WHITESPACE
+    ['2021-usz-oropharynx',
+     '2021-clb-oropharynx',
+     '2023-clb-multisite',
+     '2023-isb-multisite']
+    """
+    if where == "disk":
+        yield from _available_datasets_on_disk(year, institution, subsite)
 
 
 def load_datasets(
