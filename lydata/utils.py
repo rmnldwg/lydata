9332b031d9eef032483b61577ea46a4223a7bb19
Roman Ludwig <48687784+rmnldwg@users.noreply.github.com>
2024-06-12 16:25:54 +0200

    add modalities and replace globals with functions

--- lydata/utils.py
+++ lydata/utils.py
@@ -2,7 +2,7 @@
 
 from collections.abc import Callable
 from dataclasses import dataclass, field
-from typing import Any
+from typing import Any, Literal
 
 import pandas as pd
 
@@ -40,21 +40,53 @@ class _ColumnMap:
         return iter(self.from_short.values())
 
 
-_COLUMN_MAP = _ColumnMap.from_list([
-    _ColumnSpec("age", ("patient", "#", "age")),
-    _ColumnSpec("hpv", ("patient", "#", "hpv_status")),
-    _ColumnSpec("smoke", ("patient", "#", "nicotine_abuse")),
-    _ColumnSpec("alcohol", ("patient", "#", "alcohol_abuse")),
-    _ColumnSpec("t_stage", ("tumor"  , "1", "t_stage")),
-    _ColumnSpec("n_stage", ("patient", "#", "n_stage")),
-    _ColumnSpec("m_stage", ("patient", "#", "m_stage")),
-    _ColumnSpec("midext", ("tumor"  , "1", "extension")),
-])
+def get_default_column_map() -> _ColumnMap:
+    """Get the default column map."""
+    return _ColumnMap.from_list([
+        _ColumnSpec("age", ("patient", "#", "age")),
+        _ColumnSpec("hpv", ("patient", "#", "hpv_status")),
+        _ColumnSpec("smoke", ("patient", "#", "nicotine_abuse")),
+        _ColumnSpec("alcohol", ("patient", "#", "alcohol_abuse")),
+        _ColumnSpec("t_stage", ("tumor"  , "1", "t_stage")),
+        _ColumnSpec("n_stage", ("patient", "#", "n_stage")),
+        _ColumnSpec("m_stage", ("patient", "#", "m_stage")),
+        _ColumnSpec("midext", ("tumor"  , "1", "extension")),
+    ])
+
+
+@dataclass
+class Modality:
+    """Diagnostic modaltiy storing sensitivity and specificity."""
+
+    name: str
+    sens: float
+    spec: float
+    kind: Literal["clinical", "pathological"] = "clinical"
+
+    def __post_init__(self):
+        """Check that sensitivity and specificity are in [0, 1]."""
+        if not 0 <= self.sens <= 1:
+            raise ValueError("Sensitivity must be in [0, 1].")
+        if not 0 <= self.spec <= 1:
+            raise ValueError("Specificity must be in [0, 1].")
+
+
+def get_default_modalities() -> list[Modality]:
+    """Sensitivities and specificities of modalities. From de Bondt et al. (2007)."""
+    return [
+        Modality("CT", 0.76, 0.81),
+        Modality("MRI", 0.63, 0.81),
+        Modality("PET", 0.86, 0.79),
+        Modality("FNA", 0.98, 0.80, "pathological"),
+        Modality("diagnostic_consensus", 0.86, 0.81),
+        Modality("pathology", 1.0, 1.0, "pathological"),
+        Modality("pCT", 0.86, 0.81),
+    ]
 
 
 def main() -> None:
     """Run the main function."""
-    for col in _COLUMN_MAP:
+    for col in get_default_column_map():
         print(col)
 
 
