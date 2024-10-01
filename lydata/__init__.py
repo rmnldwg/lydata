610acaf097c2e40be7d4650b8c0f3bdb5908b6dd
Roman Ludwig <48687784+rmnldwg@users.noreply.github.com>
2024-10-01 16:55:47 +0200

    change: import useful stuff as top-level

--- lydata/__init__.py
+++ lydata/__init__.py
@@ -3,12 +3,28 @@
 import logging
 
 import lydata._version as _version
+from lydata import accessor
+from lydata.loader import (
+    available_datasets,
+    join_datasets,
+    load_dataset,
+    load_datasets,
+)
+from lydata.validator import validate_datasets
 
 __author__ = "Roman Ludwig"
 __email__ = "roman.ludwig@usz.ch"
 __uri__ = "https://github.com/rmnldwg/lydata"
-_repo = __uri__.replace("https://github.com/", "")
 __version__ = _version.__version__
+
+__all__ = [
+    "accessor",
+    "available_datasets",
+    "join_datasets",
+    "load_datasets",
+    "load_dataset",
+    "validate_datasets",
+]
 
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
